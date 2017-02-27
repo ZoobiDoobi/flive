21f711e0526bca53fa182ba7281cd5b3b1ddd566
ZoobiDoobi <bscsf12m033@gmail.com>
2017-02-27 19:58:28 +0500

    resolved url of assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-     <link href="{{base_path('css/app.css')}}" rel="stylesheet">
+     <link href="{{URL::asset('css/app.css')}}" rel="stylesheet">
    <!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
     <!-- Scripts -->
     <script>
@@ -55,7 +55,7 @@
     
 
     <!-- Scripts -->
-    <script src="{{base_path('js/app.js')}}"></script>
+    <script src="{{URL::asset('js/app.js')}}"></script>
     @yield('js-section')
 </body>
 </html>
