1d86d1972f6e33f1fe91899edfefcad02b4dce88
ZoobiDoobi <bscsf12m033@gmail.com>
2017-03-06 15:36:21 +0500

    Added a facebook login button containing fb logo

--- resources/views/facebook/connect.blade.php
+++ resources/views/facebook/connect.blade.php
@@ -8,7 +8,7 @@
             <div class="panel panel-primary">
                 <div class="panel-heading text-center"><h2>START CREATING CAMPAIGN<h2></div>
                 <div class="panel-body text-center">
-                    <a href="{{ $loginUrl }}">LOGIN WITH FACEBOOK</a>
+                    <a id="loginUrl" href="{{ $loginUrl }}"><img src="{{asset('images/fb.png')}}"> &nbsp;LOGIN WITH FACEBOOK</a>
                 </div>
             </div>
         </div>
